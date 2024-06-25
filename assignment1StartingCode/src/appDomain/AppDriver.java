6e431484d663437ea4b5fadf7c2cf8239404be27
Hoang Nam Nguyen <HoangNam.Nguyen01@edu.sait.ca>
2024-06-24 21:41:22 -0600

    Nam's commit assignment 1

--- assignment1StartingCode/src/appDomain/AppDriver.java
+++ assignment1StartingCode/src/appDomain/AppDriver.java
@@ -1,40 +1,155 @@
 package appDomain;
 
+import shapes.Shape;
+import shapes.Cone;
+import shapes.Cylinder;
+import shapes.OctagonalPrism;
+import shapes.PentagonalPrism;
+import shapes.Pyramid;
+import shapes.SquarePrism;
+import shapes.TriangularPrism;
+import sorting.SortingAlgorithms;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class AppDriver {
-
     public static void main(String[] args) {
+        if (args.length < 3) {
+            System.out.println("Usage: java -jar Sort.jar -f<file_name> -t<sort_type> -s<sort_algorithm>");
+            return;
+        }
 
-        String filename = null;
-        char sortType = ' ';
-        char algorithm = ' ';
+        String fileName = null;
+        char sortType = 'h';
+        char sortAlgorithm = 'b';
 
-        for (int i = 0; i < args.length; i++) {
-            switch (args[i].toLowerCase()) {
-                case "-f":
-                    filename = args[++i];
-                    break;
-                case "-t":
-                    sortType = args[++i].toLowerCase().charAt(0);
-                    break;
-                case "-s":
-                    algorithm = args[++i].toLowerCase().charAt(0);
-                    break;
-                default:
-                    break;
+        for (String arg : args) {
+            if (arg.startsWith("-f")) {
+                fileName = arg.substring(2);
+            } else if (arg.startsWith("-t")) {
+                sortType = arg.charAt(2);
+            } else if (arg.startsWith("-s")) {
+                sortAlgorithm = arg.charAt(2);
             }
         }
 
-        // TODO Auto-generated method stub
-        // refer to demo001 BasicFileIO.java for a simple example on how to
-        // read data from a text file
-        // refer to demo01 Test.java for an example on how to parse command
-        // line arguments and benchmarking tests
-        // refer to demo02 Student.java for comparable implementation, and
-        // NameCompare.java or GradeCompare for comparator implementations
-        // refer to demo02 KittySort.java on how to use a custom sorting
-        // algorithm on a list of comparables to sort using either the
-        // natural order (comparable) or other orders (comparators)
-        // refer to demo03 OfficeManager.java on how to create specific
-        // objects using reflection from a String
+        if (fileName == null) {
+            System.out.println("File name is required.");
+            return;
+        }
+
+        Shape[] shapes = readShapesFromFile(fileName);
+
+        if (shapes == null) {
+            System.out.println("Error reading shapes from file.");
+            return;
+        }
+
+        long startTime = System.currentTimeMillis();
+
+        switch (sortAlgorithm) {
+            case 'b':
+                SortingAlgorithms.bubbleSort(shapes);
+                break;
+            case 's':
+                SortingAlgorithms.selectionSort(shapes);
+                break;
+            case 'i':
+                SortingAlgorithms.insertionSort(shapes);
+                break;
+            case 'm':
+                SortingAlgorithms.mergeSort(shapes);
+                break;
+            case 'q':
+                SortingAlgorithms.quickSort(shapes);
+                break;
+            case 'h': // Assuming 'h' for Heap Sort as custom choice
+                SortingAlgorithms.heapSort(shapes);
+                break;
+            default:
+                System.out.println("Unknown sorting algorithm.");
+                return;
+        }
+
+        long endTime = System.currentTimeMillis();
+        System.out.println("Sorting took " + (endTime - startTime) + " milliseconds.");
+
+        printSortedShapes(shapes);
+    }
+
+    private static Shape[] readShapesFromFile(String fileName) {
+        List<Shape> shapeList = new ArrayList<>();
+
+        try (Scanner scanner = new Scanner(new File(fileName))) {
+            int numberOfShapes = scanner.nextInt(); // Read the number of shapes
+            scanner.nextLine(); // Move to the next line after reading the number
+
+            for (int i = 0; i < numberOfShapes; i++) {
+                if (scanner.hasNextLine()) {
+                    String shapeLine = scanner.nextLine();
+                    String[] shapeData = shapeLine.split(" ");
+                    String shapeType = shapeData[0];
+
+                    switch (shapeType) {
+                        case "Cylinder":
+                            double cylinderHeight = Double.parseDouble(shapeData[1]);
+                            double cylinderRadius = Double.parseDouble(shapeData[2]);
+                            shapeList.add(new Cylinder(cylinderHeight, cylinderRadius));
+                            break;
+                        case "Cone":
+                            double coneHeight = Double.parseDouble(shapeData[1]);
+                            double coneRadius = Double.parseDouble(shapeData[2]);
+                            shapeList.add(new Cone(coneHeight, coneRadius));
+                            break;
+                        case "Pyramid":
+                            double pyramidHeight = Double.parseDouble(shapeData[1]);
+                            double pyramidEdgeLength = Double.parseDouble(shapeData[2]);
+                            shapeList.add(new Pyramid(pyramidHeight, pyramidEdgeLength));
+                            break;
+                        case "SquarePrism":
+                            double squarePrismHeight = Double.parseDouble(shapeData[1]);
+                            double squarePrismEdgeLength = Double.parseDouble(shapeData[2]);
+                            shapeList.add(new SquarePrism(squarePrismHeight, squarePrismEdgeLength));
+                            break;
+                        case "TriangularPrism":
+                            double triangularPrismHeight = Double.parseDouble(shapeData[1]);
+                            double triangularPrismEdgeLength = Double.parseDouble(shapeData[2]);
+                            shapeList.add(new TriangularPrism(triangularPrismHeight, triangularPrismEdgeLength));
+                            break;
+                        case "PentagonalPrism":
+                            double pentagonalPrismHeight = Double.parseDouble(shapeData[1]);
+                            double pentagonalPrismEdgeLength = Double.parseDouble(shapeData[2]);
+                            shapeList.add(new PentagonalPrism(pentagonalPrismHeight, pentagonalPrismEdgeLength));
+                            break;
+                        case "OctagonalPrism":
+                            double octagonalPrismHeight = Double.parseDouble(shapeData[1]);
+                            double octagonalPrismEdgeLength = Double.parseDouble(shapeData[2]);
+                            shapeList.add(new OctagonalPrism(octagonalPrismHeight, octagonalPrismEdgeLength));
+                            break;
+                        default:
+                            System.out.println("Unknown shape type: " + shapeType);
+                            break;
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + fileName);
+            e.printStackTrace();
+        }
+
+        return shapeList.toArray(new Shape[0]);
+    }
+
+    private static void printSortedShapes(Shape[] shapes) {
+        if (shapes.length > 0) {
+            System.out.println("First Shape: " + shapes[0]);
+            for (int i = 1000; i < shapes.length; i += 1000) {
+                System.out.println("Shape at index " + i + ": " + shapes[i]);
+            }
+            System.out.println("Last Shape: " + shapes[shapes.length - 1]);
+        }
     }
 }
